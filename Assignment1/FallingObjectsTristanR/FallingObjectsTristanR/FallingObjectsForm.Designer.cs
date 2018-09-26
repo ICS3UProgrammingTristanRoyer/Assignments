6f399e480e7317cc0ce3b5d09cc04c358e79f11d
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-09-25 23:00:07 -0400

    Initial commit

---
+++ Assignment1/FallingObjectsTristanR/FallingObjectsTristanR/FallingObjectsForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FallingObjectsTristanR
+{
+    partial class FallingObjectsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.txtTime = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.lblHeight = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtTime
+			// 
+			this.txtTime.Location = new System.Drawing.Point(350, 37);
+			this.txtTime.Name = "txtTime";
+			this.txtTime.Size = new System.Drawing.Size(100, 20);
+			this.txtTime.TabIndex = 0;
+			this.txtTime.TextChanged += new System.EventHandler(this.txtTime_TextChanged);
+			this.txtTime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTime_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(27, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(276, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Enter the time (in seconds) since you released the object.";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(50, 78);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Calculate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(27, 136);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(200, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Height of the object above the ground is:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(47, 136);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 13);
+			this.label3.TabIndex = 4;
+			// 
+			// lblHeight
+			// 
+			this.lblHeight.AutoSize = true;
+			this.lblHeight.Location = new System.Drawing.Point(284, 136);
+			this.lblHeight.Name = "lblHeight";
+			this.lblHeight.Size = new System.Drawing.Size(13, 13);
+			this.lblHeight.TabIndex = 5;
+			this.lblHeight.Text = "--";
+			// 
+			// FallingObjectsForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblHeight);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtTime);
+			this.Name = "FallingObjectsForm";
+			this.Text = "Falling Objects";
+			this.Load += new System.EventHandler(this.FallingObjectsForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHeight;
+    }
+}
+
---
+++ Assignment1/FallingObjectsTristanR/FallingObjectsTristanR/FallingObjectsForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsTristanR
+{
+	public partial class FallingObjectsForm : Form
+	{
+		public FallingObjectsForm()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			// This provides an error message if the user does not enter a value
+			if (txtTime.Text == "")
+			{
+				this.lblHeight.Text = "Error (enter valid value)";
+			}
+			else
+			{
+				//Declare local variables
+				double time, height;
+				// convert the string from the text box to a double
+				time = double.Parse(txtTime.Text);
+
+				// Calculate the height above the ground
+				height = 100 - (0.5 * 9.81 * Math.Pow(time, 2));
+
+				// Insert the height to it's respective label
+
+				this.lblHeight.Text = Convert.ToString(height) + " meters";
+
+				if (time > 4.51)
+				{
+					// if the ball has already reached the ground it will display the appropriate text.
+					this.lblHeight.Text = "The ball has already eached the ground";
+				}
+
+
+
+
+
+
+
+
+
+			}
+		}
+
+			private void txtTime_TextChanged(object sender, EventArgs e)
+			{
+
+
+			}
+
+			private void txtTime_KeyPress(object sender, KeyPressEventArgs e)
+			{
+			// allows only digits and decimals to be typed into the textbox (source : StackOverflow)
+			if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+					
+					e.Handled = true;
+
+
+			}
+
+		private void FallingObjectsForm_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+	}
